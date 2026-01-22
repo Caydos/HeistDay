28b97af8dc35e5509cab847ac34a0cd56f60206f
Leikyz <leikyz17@outlook.com>
2026-01-22 17:27:55 +0100

    feat: add dedicated server, EOS connection and client connection

---
+++ Source/FinalGame/Network/EOSNetworkSubsystem.cpp
@@ -0,0 +1,91 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EOSNetworkSubsystem.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSubsystemUtils.h"
+
+void UEOSNetworkSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (Subsystem)
+	{
+		IdentityInterface = Subsystem->GetIdentityInterface();
+		/*SessionInterface = Subsystem->GetSessionInterface();*/
+
+		if (IsRunningDedicatedServer())
+		{
+			UE_LOG(LogTemp, Warning, TEXT("SERVER : EOS Dedicated server initialization ..."));
+			StartServerEOS();
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("CLIENT : Client initialization..."));
+
+			FTimerHandle TimerHandle;
+			GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UEOSNetworkSubsystem::LoginWithDevAuth, 5.0f, false);
+		}
+	}
+}
+void UEOSNetworkSubsystem::StartServerEOS()
+{
+	if (!IdentityInterface.IsValid()) return;
+
+	FOnlineAccountCredentials Credentials;
+	Credentials.Type = TEXT("accountportal"); 
+
+	IdentityInterface->AddOnLoginCompleteDelegate_Handle(0,
+		FOnLoginCompleteDelegate::CreateUObject(this, &ThisClass::OnServerLoginComplete));
+
+	UE_LOG(LogTemp, Warning, TEXT("SERVEUR : Tentative d'authentification auprès d'Epic..."));
+	IdentityInterface->Login(0, Credentials);
+}
+
+void UEOSNetworkSubsystem::OnServerLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error)
+{
+	if (bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("SERVEUR : Authentification EOS réussie !"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("SERVEUR : Échec de l'authentification : %s"), *Error);
+	}
+}
+void UEOSNetworkSubsystem::LoginWithDevAuth()
+{
+	if (!IdentityInterface.IsValid()) return;
+
+	FOnlineAccountCredentials Credentials;
+
+	Credentials.Type = TEXT("developer");
+	Credentials.Id = TEXT("localhost:8081");
+	Credentials.Token = TEXT("DevUser");
+
+	UE_LOG(LogTemp, Warning, TEXT("Vérification avant Login : Type='%s', Id='%s', Token='%s'"),
+		*Credentials.Type, *Credentials.Id, *Credentials.Token);
+
+	IdentityInterface->AddOnLoginCompleteDelegate_Handle(0,
+		FOnLoginCompleteDelegate::CreateUObject(this, &ThisClass::OnLoginComplete));
+
+	if (!IdentityInterface->Login(0, Credentials))
+	{
+		UE_LOG(LogTemp, Error, TEXT("L'appel à IdentityInterface->Login a échoué immédiatement !"));
+	}
+}
+
+void UEOSNetworkSubsystem::OnLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error)
+{
+	if (bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("=== LOGIN RÉUSSI ! ==="));
+		UE_LOG(LogTemp, Warning, TEXT("Utilisateur EOS connecté : %s"), *UserId.ToString());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("=== ÉCHEC DU LOGIN EOS ==="));
+		UE_LOG(LogTemp, Error, TEXT("Raison de l'erreur : %s"), *Error);
+	}
+}
---
+++ Source/FinalGame/Network/EOSNetworkSubsystem.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineIdentityInterface.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "EOSNetworkSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALGAME_API UEOSNetworkSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	// Login with Dev Auth Tool
+	void LoginWithDevAuth();
+	void StartServerEOS();
+
+protected:
+	// Callback when login is complete
+	void OnLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error);
+	void OnServerLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error);
+
+private:
+	// Online Identity Interface pointer
+	IOnlineIdentityPtr IdentityInterface;
+};
+	
