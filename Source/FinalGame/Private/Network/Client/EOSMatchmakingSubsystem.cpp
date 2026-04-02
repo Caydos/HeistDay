cb59d6093e7df0f57d6de75a171cea937e6234c4
Leikyz <leikyz17@outlook.com>
2026-04-02 13:48:23 +0200

    feat: change IP server with IPV4

--- Source/FinalGame/Private/Network/Client/EOSMatchmakingSubsystem.cpp
+++ Source/FinalGame/Private/Network/Client/EOSMatchmakingSubsystem.cpp
@@ -1,101 +1,101 @@
-#include "Network/Client/EOSMatchmakingSubsystem.h"
-#include "Network/Client/EOSIdentitySubsystem.h"
-#include "Network/Client/EOSLobbySubsystem.h"
-
-using namespace UE::Online;
-
-void UEOSMatchmakingSubsystem::FindMatch(int32 PartySize)
-{
-	OnStatusChanged.Broadcast(TEXT("Searching for opponents..."));
-	SearchForLobbies();
-}
-
-void UEOSMatchmakingSubsystem::SearchForLobbies()
-{
-	IOnlineServicesPtr Services = UE::Online::GetServices();
-	UEOSIdentitySubsystem* Identity = GetGameInstance()->GetSubsystem<UEOSIdentitySubsystem>();
-
-	if (Services && Identity)
-	{
-		ILobbiesPtr Lobbies = Services->GetLobbiesInterface();
-		FFindLobbies::Params Params;
-		Params.LocalAccountId = Identity->GetLocalAccountId();
-		Params.MaxResults = 10;
-
-		// Find Lobbies tagged as 2v2
-		FFindLobbySearchFilter Filter;
-		Filter.AttributeName = FSchemaAttributeId(TEXT("GameMode"));
-		Filter.ComparisonOp = ESchemaAttributeComparisonOp::Equals;
-		Filter.ComparisonValue = FSchemaVariant(FString(TEXT("2v2")));
-		Params.Filters.Add(Filter);
-
-		Lobbies->FindLobbies(MoveTemp(Params)).OnComplete(this, &UEOSMatchmakingSubsystem::OnSearchCompleted);
-	}
-}
-
-void UEOSMatchmakingSubsystem::OnSearchCompleted(const TOnlineResult<FFindLobbies>& Result)
-{
-	if (Result.IsOk() && Result.GetOkValue().Lobbies.Num() > 0)
-	{
-		OnStatusChanged.Broadcast(TEXT("Match found! Joining..."));
-
-		// We found an opponent's lobby! Let's join it.
-		IOnlineServicesPtr Services = UE::Online::GetServices();
-		UEOSIdentitySubsystem* Identity = GetGameInstance()->GetSubsystem<UEOSIdentitySubsystem>();
-
-		FJoinLobby::Params Params;
-		Params.LocalAccountId = Identity->GetLocalAccountId();
-		Params.LocalName = FName(TEXT("PartyLobby")); // Must match the name in CreateLobby!
-		Params.LobbyId = Result.GetOkValue().Lobbies[0]->LobbyId;
-		Params.bPresenceEnabled = true;
-
-		Services->GetLobbiesInterface()->JoinLobby(MoveTemp(Params)).OnComplete(this, &UEOSMatchmakingSubsystem::OnJoinCompleted);
-	}
-	else
-	{
-		// No opponents found. We will host the match by making our Party Lobby public!
-		MakePartyLobbyPublic();
-	}
-}
-
-void UEOSMatchmakingSubsystem::MakePartyLobbyPublic()
-{
-	IOnlineServicesPtr Services = UE::Online::GetServices();
-	UEOSIdentitySubsystem* Identity = GetGameInstance()->GetSubsystem<UEOSIdentitySubsystem>();
-	UEOSLobbySubsystem* LobbySub = GetGameInstance()->GetSubsystem<UEOSLobbySubsystem>();
-
-	if (Services && Identity && LobbySub->IsInLobby())
-	{
-		FModifyLobbyAttributes::Params Params;
-		Params.LocalAccountId = Identity->GetLocalAccountId();
-		Params.LobbyId = LobbySub->CurrentLobbyId; // Use the Lobby we are already inside!
-
-		// Add the 2v2 tag so others can find us
-		Params.UpdatedAttributes.Add(FSchemaAttributeId(TEXT("GameMode")), FSchemaVariant(FString(TEXT("2v2"))));
-
-		Services->GetLobbiesInterface()->ModifyLobbyAttributes(MoveTemp(Params));
-
-		// Make the lobby Publicly Searchable
-		FModifyLobbyJoinPolicy::Params JoinParams;
-		JoinParams.LocalAccountId = Identity->GetLocalAccountId();
-		JoinParams.LobbyId = LobbySub->CurrentLobbyId;
-		JoinParams.JoinPolicy = ELobbyJoinPolicy::PublicAdvertised;
-
-		Services->GetLobbiesInterface()->ModifyLobbyJoinPolicy(MoveTemp(JoinParams));
-
-		OnStatusChanged.Broadcast(TEXT("Waiting for opponents..."));
-	}
-}
-
-void UEOSMatchmakingSubsystem::OnJoinCompleted(const TOnlineResult<FJoinLobby>& Result)
-{
-	if (Result.IsOk())
-	{
-		OnStatusChanged.Broadcast(TEXT("Successfully joined the match!"));
-		// The teleport is handled automatically by OnLobbyMemberJoined in EOSLobbySubsystem!
-	}
-	else
-	{
-		OnStatusChanged.Broadcast(TEXT("Failed to join match."));
-	}
-}
+//#include "Network/Client/EOSMatchmakingSubsystem.h"
+//#include "Network/Client/EOSIdentitySubsystem.h"
+//#include "Network/Client/EOSLobbySubsystem.h"
+//
+//using namespace UE::Online;
+//
+//void UEOSMatchmakingSubsystem::FindMatch(int32 PartySize)
+//{
+//	OnStatusChanged.Broadcast(TEXT("Searching for opponents..."));
+//	SearchForLobbies();
+//}
+//
+//void UEOSMatchmakingSubsystem::SearchForLobbies()
+//{
+//	IOnlineServicesPtr Services = UE::Online::GetServices();
+//	UEOSIdentitySubsystem* Identity = GetGameInstance()->GetSubsystem<UEOSIdentitySubsystem>();
+//
+//	if (Services && Identity)
+//	{
+//		ILobbiesPtr Lobbies = Services->GetLobbiesInterface();
+//		FFindLobbies::Params Params;
+//		Params.LocalAccountId = Identity->GetLocalAccountId();
+//		Params.MaxResults = 10;
+//
+//		// Find Lobbies tagged as 2v2
+//		FFindLobbySearchFilter Filter;
+//		Filter.AttributeName = FSchemaAttributeId(TEXT("GameMode"));
+//		Filter.ComparisonOp = ESchemaAttributeComparisonOp::Equals;
+//		Filter.ComparisonValue = FSchemaVariant(FString(TEXT("2v2")));
+//		Params.Filters.Add(Filter);
+//
+//		Lobbies->FindLobbies(MoveTemp(Params)).OnComplete(this, &UEOSMatchmakingSubsystem::OnSearchCompleted);
+//	}
+//}
+//
+//void UEOSMatchmakingSubsystem::OnSearchCompleted(const TOnlineResult<FFindLobbies>& Result)
+//{
+//	if (Result.IsOk() && Result.GetOkValue().Lobbies.Num() > 0)
+//	{
+//		OnStatusChanged.Broadcast(TEXT("Match found! Joining..."));
+//
+//		// We found an opponent's lobby! Let's join it.
+//		IOnlineServicesPtr Services = UE::Online::GetServices();
+//		UEOSIdentitySubsystem* Identity = GetGameInstance()->GetSubsystem<UEOSIdentitySubsystem>();
+//
+//		FJoinLobby::Params Params;
+//		Params.LocalAccountId = Identity->GetLocalAccountId();
+//		Params.LocalName = FName(TEXT("PartyLobby")); // Must match the name in CreateLobby!
+//		Params.LobbyId = Result.GetOkValue().Lobbies[0]->LobbyId;
+//		Params.bPresenceEnabled = true;
+//
+//		Services->GetLobbiesInterface()->JoinLobby(MoveTemp(Params)).OnComplete(this, &UEOSMatchmakingSubsystem::OnJoinCompleted);
+//	}
+//	else
+//	{
+//		// No opponents found. We will host the match by making our Party Lobby public!
+//		MakePartyLobbyPublic();
+//	}
+//}
+//
+//void UEOSMatchmakingSubsystem::MakePartyLobbyPublic()
+//{
+//	IOnlineServicesPtr Services = UE::Online::GetServices();
+//	UEOSIdentitySubsystem* Identity = GetGameInstance()->GetSubsystem<UEOSIdentitySubsystem>();
+//	UEOSLobbySubsystem* LobbySub = GetGameInstance()->GetSubsystem<UEOSLobbySubsystem>();
+//
+//	if (Services && Identity && LobbySub->IsInLobby())
+//	{
+//		FModifyLobbyAttributes::Params Params;
+//		Params.LocalAccountId = Identity->GetLocalAccountId();
+//		//Params.LobbyId = LobbySub->CurrentLobbyId; // Use the Lobby we are already inside!
+//
+//		// Add the 2v2 tag so others can find us
+//		Params.UpdatedAttributes.Add(FSchemaAttributeId(TEXT("GameMode")), FSchemaVariant(FString(TEXT("2v2"))));
+//
+//		Services->GetLobbiesInterface()->ModifyLobbyAttributes(MoveTemp(Params));
+//
+//		// Make the lobby Publicly Searchable
+//		FModifyLobbyJoinPolicy::Params JoinParams;
+//		JoinParams.LocalAccountId = Identity->GetLocalAccountId();
+//		/*JoinParams.LobbyId = LobbySub->CurrentLobbyId;*/
+//		JoinParams.JoinPolicy = ELobbyJoinPolicy::PublicAdvertised;
+//
+//		Services->GetLobbiesInterface()->ModifyLobbyJoinPolicy(MoveTemp(JoinParams));
+//
+//		OnStatusChanged.Broadcast(TEXT("Waiting for opponents..."));
+//	}
+//}
+//
+//void UEOSMatchmakingSubsystem::OnJoinCompleted(const TOnlineResult<FJoinLobby>& Result)
+//{
+//	if (Result.IsOk())
+//	{
+//		OnStatusChanged.Broadcast(TEXT("Successfully joined the match!"));
+//		// The teleport is handled automatically by OnLobbyMemberJoined in EOSLobbySubsystem!
+//	}
+//	else
+//	{
+//		OnStatusChanged.Broadcast(TEXT("Failed to join match."));
+//	}
+//}
--- Source/FinalGame/Public/Network/Client/EOSMatchmakingSubsystem.h
+++ Source/FinalGame/Public/Network/Client/EOSMatchmakingSubsystem.h
@@ -1,29 +1,29 @@
-#pragma once
-
-#include "CoreMinimal.h"
-#include "Subsystems/GameInstanceSubsystem.h"
-#include "Online/OnlineServices.h"
-#include "Online/Lobbies.h"
-#include "EOSMatchmakingSubsystem.generated.h"
-
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMatchmakingStatusChanged, FString, StatusMessage);
-
-UCLASS()
-class FINALGAME_API UEOSMatchmakingSubsystem : public UGameInstanceSubsystem
-{
-	GENERATED_BODY()
-
-public:
-	UFUNCTION(BlueprintCallable, Category = "EOS|Matchmaking")
-	void FindMatch(int32 PartySize);
-
-	UPROPERTY(BlueprintAssignable, Category = "EOS|Matchmaking")
-	FOnMatchmakingStatusChanged OnStatusChanged;
-
-private:
-	void SearchForLobbies();
-	void MakePartyLobbyPublic();
-
-	void OnSearchCompleted(const UE::Online::TOnlineResult<UE::Online::FFindLobbies>& Result);
-	void OnJoinCompleted(const UE::Online::TOnlineResult<UE::Online::FJoinLobby>& Result);
-};
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "Subsystems/GameInstanceSubsystem.h"
+//#include "Online/OnlineServices.h"
+//#include "Online/Lobbies.h"
+//#include "EOSMatchmakingSubsystem.generated.h"
+//
+//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMatchmakingStatusChanged, FString, StatusMessage);
+//
+//UCLASS()
+//class FINALGAME_API UEOSMatchmakingSubsystem : public UGameInstanceSubsystem
+//{
+//	GENERATED_BODY()
+//
+//public:
+//	UFUNCTION(BlueprintCallable, Category = "EOS|Matchmaking")
+//	void FindMatch(int32 PartySize);
+//
+//	UPROPERTY(BlueprintAssignable, Category = "EOS|Matchmaking")
+//	FOnMatchmakingStatusChanged OnStatusChanged;
+//
+//private:
+//	void SearchForLobbies();
+//	void MakePartyLobbyPublic();
+//
+//	void OnSearchCompleted(const UE::Online::TOnlineResult<UE::Online::FFindLobbies>& Result);
+//	void OnJoinCompleted(const UE::Online::TOnlineResult<UE::Online::FJoinLobby>& Result);
+//};
